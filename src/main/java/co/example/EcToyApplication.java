70a45e96a1fd0e43d88b22d93a8e5e5069ef06bd
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2019-12-05 10:52:23 +0900

    first commit

---
+++ src/main/java/co/example/EcToyApplication.java
@@ -0,0 +1,13 @@
+package co.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcToyApplication.class, args);
+	}
+
+}
